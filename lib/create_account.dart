ac7389038f56bbc39479640e9e6591c2749c773a
nakajima <2190087@ecccomp.com>
2022-10-13 09:47:58 +0900

    担当画面追加

---
+++ lib/create_account.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class C_Account extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/create_schedule.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class C_Schedule extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
