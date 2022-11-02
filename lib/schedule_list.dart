65608bb370f4c6e2b0b6f41e298db3bd33c5d170
nakajima <2190087@ecccomp.com>
2022-11-02 10:32:06 +0900

    'Firebase認証完成'

--- lib/schedule_list.dart
+++ lib/schedule_list.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class S_list extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: Text("スケジュール一覧画面"));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("スケジュール一覧画面"),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/c_schedule');
+                },
+                child: Text("スケジュール作成")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/map');
+                },
+                child: Text("マップ")),
+          ],
+        ),
+      ),
+    );
   }
 }
