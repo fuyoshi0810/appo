f1bf792ef8ca359671314cf9ae782eda3ab877d8
Fujio_Tsuyohi <2190079@ecccomp.com>
2022-11-07 15:02:24 +0900

    map画面遷移完了

--- lib/map.dart
+++ lib/map.dart
@@ -3,104 +3,91 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 
-void main() => runApp(const Map());
-
-class Map extends StatelessWidget {
-  const Map({Key? key}) : super(key: key);
-
+class Map extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Google Maps Demo',
-      home: MapSample(),
-    );
-  }
+  _MapBody createState() => _MapBody();
 }
 
-class MapSample extends StatefulWidget {
-  const MapSample({Key? key}) : super(key: key);
+class _MapBody extends State<Map> {
+  Completer<GoogleMapController> _controller = Completer();
 
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Position? currentPosition;
-  late GoogleMapController _controller;
-  late StreamSubscription<Position> positionStream;
-  //初期位置
-  final CameraPosition _kGooglePlex = const CameraPosition(
-    target: LatLng(43.0686606, 141.3485613),
-    zoom: 14,
-  );
-
-  final LocationSettings locationSettings = const LocationSettings(
-    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
-    distanceFilter: 100,
-  );
+  late LatLng _initialPosition;
+  late bool _loading;
 
   @override
   void initState() {
     super.initState();
+    _loading = true;
+    _getUserLocation();
+  }
 
-    //位置情報が許可されていない時に許可をリクエストする
-    Future(() async {
-      LocationPermission permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        await Geolocator.requestPermission();
-      }
-    });
-
-    //現在位置を更新し続ける
-    positionStream =
-        Geolocator.getPositionStream(locationSettings: locationSettings)
-            .listen((Position? position) {
-      currentPosition = position;
-      print(position == null
-          ? 'Unknown'
-          : '${position.latitude.toString()}, ${position.longitude.toString()}');
+  void _getUserLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      _loading = false;
+      print(position);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        drawer: Drawer(
-            child: ListView(
-          children: [
-            DrawerHeader(
-                decoration: BoxDecoration(color: Colors.yellowAccent),
-                child: Text("My Home Page")),
-            ListTile(
-              title: Text("スケジュール一覧"),
+      appBar: AppBar(),
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          DrawerHeader(
+              decoration: BoxDecoration(color: Colors.yellowAccent),
+              child: Text("各画面遷移")),
+          ListTile(
+            title: Text("スケジュール一覧"),
+            onTap: () {
+              Navigator.pushNamed(context, '/s_list');
+            },
+          ),
+          ListTile(
+            title: Text("グループ設定画面"),
+            onTap: () {
+              Navigator.pushNamed(context, '/s_group');
+            },
+          ),
+          ListTile(
+              title: Text("退出"),
               onTap: () {
-                Navigator.pushNamed(context, '/s_list');
-              },
+                Navigator.pushNamed(context, '/c_group');
+              }),
+        ],
+      )),
+      body: _loading
+          ? CircularProgressIndicator()
+          : SafeArea(
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  SizedBox(
+                    child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: _initialPosition,
+                        zoom: 14.4746,
+                      ),
+                      onMapCreated: (GoogleMapController controller) {
+                        _controller.complete(controller);
+                      },
+                      // markers: _createMarker(),
+                      myLocationEnabled: true,
+                      myLocationButtonEnabled: true,
+                      mapToolbarEnabled: false,
+                      buildingsEnabled: true,
+                      onTap: (LatLng latLang) {
+                        print('Clicked: $latLang');
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
-            ListTile(
-              title: Text("グループ設定画面"),
-              onTap: () {
-                Navigator.pushNamed(context, '/s_group');
-              },
-            ),
-          ],
-        )),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              SizedBox(
-                  width: double.infinity,
-                  height: 500,
-                  child: GoogleMap(
-                    mapType: MapType.normal,
-                    initialCameraPosition: _kGooglePlex,
-                    myLocationEnabled: true, //現在位置をマップ上に表示
-                    onMapCreated: (GoogleMapController controller) {
-                      _controller = controller;
-                    },
-                  ))
-            ])));
+    );
   }
 }
